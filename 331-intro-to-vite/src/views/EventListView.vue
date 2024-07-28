9b3bc02a2b73f744e0a362b081e921ffcd5792da
Amber <1448990064@qq.com>
2024-07-28 17:08:34 +0700

    2.5 & 2.7
    previous 2.5 is 2.4

--- 331-intro-to-vite/src/views/EventListView.vue
+++ 331-intro-to-vite/src/views/EventListView.vue
@@ -2,7 +2,7 @@
 import EventCard from '@/components/EventCard.vue'
 import CategoryCard from '@/components/CategoryCard.vue'
 import { type Event} from '@/types'
-import { ref, onMounted,computed } from 'vue'
+import { ref, onMounted, computed, watchEffect } from 'vue'
 import EventService from '@/services/EventService'
 
 const events = ref<Event[] | null >(null)
@@ -14,14 +14,16 @@ const props = defineProps({
 })
 const page = computed(() => props.page)
 onMounted(() => {
-  EventService.getEvents(2, page.value)
-    .then((response) => {
-      //console.log(response.data)
+  watchEffect (() => {
+    events.value = null
+    EventService.getEvents(2, page.value)
+    .then ((response) => {
       events.value = response.data
     })
     .catch((error) => {
       console.error('There was an error!', error)
     })
+  })
 })
 </script>
 
@@ -32,6 +34,15 @@ onMounted(() => {
     <EventCard v-for="event in events" :key="event.id" :event="event" />
     <CategoryCard v-for="event in events" :key="event.id" :event="event" />
   </div>
+  <RouterLink
+    :to="{ name: 'event-list-view', query: { page: page - 1 }}"
+    rel="prev"
+    v-if="page !=1"
+    >Prev Page</RouterLink>
+
+    <RouterLink :to="{ name: 'event-list-view', query: { page: page+1 }}"
+    rel="next"
+    >Next Page</RouterLink>
 </template>
 
 <style scoped>
